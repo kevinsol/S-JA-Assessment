52e7c56a24306e40c74bfb428395ab0b2c47e846
kevinsol <70930446+kevinsol@users.noreply.github.com>
2020-09-07 23:47:23 -0600

    Add files via upload

--- Python-Scripts/modules/load_db_redshift.py
+++ Python-Scripts/modules/load_db_redshift.py
@@ -20,12 +20,63 @@ def load_data_to_redshift():
     cur = conn.cursor()
     S3 = config.get('AWS','S3_BUCKETR')
     ARN = config.get('IAM_ROLE','ARN')
-    clean_query = ["TRUNCATE TABLE dim_city;"]
+    clean_query = ["TRUNCATE TABLE dim_applicant;",
+					"TRUNCATE TABLE dim_benefit;",
+					"TRUNCATE TABLE dim_city;",
+					"TRUNCATE TABLE dim_company;",
+					"TRUNCATE TABLE dim_jobpost;",
+					"TRUNCATE TABLE dim_jobtitle;",
+					"TRUNCATE TABLE dim_skill;",
+					"TRUNCATE TABLE dim_status;",
+					"TRUNCATE TABLE bridge_applicant_skills;",
+					"TRUNCATE TABLE bridge_jobpost_benefits;",
+					"TRUNCATE TABLE fact_job_applications;"]
 
-    load_query = [("""  COPY dim_city from '{}'
+    load_query = [("""  COPY dim_applicant from '{}'
                                iam_role {}
                                FORMAT AS PARQUET
-    """).format(S3+"/presentation/jobs/dim_city",ARN)]
+    """).format(S3+"/presentation/jobs/dim_applicant",ARN),
+	("""  COPY dim_benefit from '{}'
+                               iam_role {}
+                               FORMAT AS PARQUET
+    """).format(S3+"/presentation/jobs/dim_benefit",ARN),
+	("""  COPY dim_city from '{}'
+                               iam_role {}
+                               FORMAT AS PARQUET
+    """).format(S3+"/presentation/jobs/dim_city",ARN),
+	("""  COPY dim_company from '{}'
+                               iam_role {}
+                               FORMAT AS PARQUET
+    """).format(S3+"/presentation/jobs/dim_company",ARN),
+	("""  COPY dim_jobpost from '{}'
+                               iam_role {}
+                               FORMAT AS PARQUET
+    """).format(S3+"/presentation/jobs/dim_jobpost",ARN),
+	("""  COPY dim_jobtitle from '{}'
+                               iam_role {}
+                               FORMAT AS PARQUET
+    """).format(S3+"/presentation/jobs/dim_jobtitle",ARN),
+	("""  COPY dim_skill from '{}'
+                               iam_role {}
+                               FORMAT AS PARQUET
+    """).format(S3+"/presentation/jobs/dim_skill",ARN),
+	("""  COPY dim_status from '{}'
+                               iam_role {}
+                               FORMAT AS PARQUET
+    """).format(S3+"/presentation/jobs/dim_status",ARN),
+	("""  COPY bridge_applicant_skills from '{}'
+                               iam_role {}
+                               FORMAT AS PARQUET
+    """).format(S3+"/presentation/jobs/bridge_applicant_skills",ARN),
+	("""  COPY bridge_jobpost_benefits from '{}'
+                               iam_role {}
+                               FORMAT AS PARQUET
+    """).format(S3+"/presentation/jobs/bridge_jobpost_benefits",ARN),
+	("""  COPY fact_job_applications from '{}'
+                               iam_role {}
+                               FORMAT AS PARQUET
+    """).format(S3+"/presentation/jobs/fact_job_applications",ARN)]
+	
     # Clean data tables
     for query in clean_query:
         cur.execute(query)
